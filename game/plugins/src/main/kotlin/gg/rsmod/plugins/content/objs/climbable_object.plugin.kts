6a47a46ee3b45c40477903fec28d49180bff049d
Tyluur <itstyluur@icloud.com>
2023-02-03 18:46:02 -0500

    Revert "feat: addition of kotlin-inline-logger to gradle deps"
    This reverts commit 3c90f30bed310a3886d9991c317fc3636979cf7d.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/climbable_object.plugin.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/objs/climbable_object.plugin.kts
@@ -1,12 +1,8 @@
 package gg.rsmod.plugins.content.objs
 
-import com.github.michaelbull.logging.InlineLogger
-
 private val CLIMB_UP_ANIMATION = 828
 
 private val CLIMB_DOWN_ANIMATION = 827
-
-private val logger = InlineLogger()
 
 /**
  * The base function for climbing
@@ -18,15 +14,5 @@ fun climb(player: Player, animation: Int, destination: Tile, vararg messages: St
         this.wait(2)
         player.moveTo(destination)
         messages.forEach(player::message)
-    }
-}
-
-for (id in 0..world.definitions.getCount(ObjectDef::class.java)) {
-    val options = world.definitions.get(ObjectDef::class.java, id).options
-    if (options.contains("Climb-up")) {
-        on_obj_option(obj = id, option = "Climb-up") {
-            logger.info { "Object = ${this.pawn}" }
-            // ... do code
-        }
     }
 }
